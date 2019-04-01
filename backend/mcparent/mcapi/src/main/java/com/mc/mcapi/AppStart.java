b73937f3396e7501520cd9b369475041f3684769
zhangxinxiu <zhangxinxiu@58.com>
2019-04-01 21:11:10 +0800

    fix:dbconnect

--- backend/mcparent/mcapi/src/main/java/com/mc/mcapi/AppStart.java
+++ backend/mcparent/mcapi/src/main/java/com/mc/mcapi/AppStart.java
@@ -3,6 +3,7 @@ package com.mc.mcapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class AppStart {
     public static void main(String[] args){
